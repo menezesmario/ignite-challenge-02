eb997f5d8e3bdf34b69aa9301f5e678ddee071b7
Mário Menezes <mario082@gmail.com>
2022-07-01 07:59:24 -0300

    sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { api } from './services/api';
 import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { SideBar } from './components/SideBar';
 
 
 interface GenreResponseProps {
@@ -22,40 +23,13 @@ interface GenreResponseProps {
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
-  
-
-  
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
-
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-    console.log(id)
-  }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
+      <SideBar/>
 
-      </nav>
+      
       <Content
         id={selectedGenreId} 
         name={'action'} 
